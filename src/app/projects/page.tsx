c11366f762bddef396e651c12ea6ee715d39610b
Scottc483 <scott.c19@live.com>
2023-05-19 00:55:54 -0700

    removed useEffect from page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,18 +1,8 @@
-'use client'
+'use '
 
 import React, {useEffect} from 'react'
 
 const page = () => {
-
-  useEffect(() => {
-    
-    if (typeof window !== 'undefined') {
-      console.log('window loaded');
-      const loader = document.getElementById('globalLoader');
-      if (loader)
-        loader.remove();
-    }
-  }, []);
 
   return (
     <div>
