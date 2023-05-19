1f68cdfe77daf5771ac1a06d3f0301fe5d2be39f
Scottc483 <scott.c19@live.com>
2023-05-19 00:59:43 -0700

    removed script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ export const metadata = {
   description: "Scott Croin | Web Developer",
   author : "Scott Croin",
 };
-import styles from './technologies.module.css'
+
 
 export default function RootLayout({
   children,
@@ -45,7 +45,6 @@ export default function RootLayout({
         <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
         <meta name="msapplication-TileColor" content="#da532c" />
         <meta name="theme-color" content="#ffffff" />
-        <script src="https://kit.fontawesome.com/8052e1056c.js" crossOrigin="anonymous"></script>
       </Head>
       <body className="bg-darkGrey">
         <main className={inter.className}>
