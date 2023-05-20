13ff039c8cfee19c624598c4b9f48bd41e0526e3
Scottc483 <scott.c19@live.com>
2023-05-20 09:34:23 -0700

    Project card teasers

--- src/components/ProjectIntro.tsx
+++ src/components/ProjectIntro.tsx
@@ -1,28 +1,42 @@
 import React from "react";
 import Link from "next/link";
+import ProjectCard from "./partials/ProjectCard";
 
 const ProjectIntro = () => {
   return (
     <>
-      <div className="flex flex-col   p-16 max-w-5xl">
-        <h2 className="text-xl sm:text-4xl md:text-2xl font-semi-bold mb-4">
+      <div className="flex flex-col p-16 max-w-5xl">
+      <h2 className="text-xl sm:text-4xl md:text-2xl font-semi-bold mb-4 pl-6">
           Take a Look at my
         </h2>
-        <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 uppercase">
+        <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 uppercase pl-6">
           Projects
         </h2>
-        <p className="text-lg mb-8">
-        Explore a collection of my projects, including an Event Management mobile app, a Ticket Purchasing and Management site, and a Carbon Emissions Calculator for daily commutes. Visit my projects page to learn more.
-
-        </p>
-        <div className="">
-        <Link
-          href="/projects"
-          className="px-8 py-3 offWhite bg-darkGrey text-xs rounded-md font-semibold uppercase transition duration-300"
-        >
-          View Projects
-        </Link>
+        <div className="flex flex-col items-center">
+          <ProjectCard
+            title="Event Management Mobile App"
+            description="An innovative mobile app built with React Native for efficient event management."
+            link="/projects/#event-management"
+            image=""
+          />
+          <ProjectCard
+            title="Ticket Purchasing and Management Site"
+            description="A convenient website developed with ASP.NET for easy ticket purchasing and management."
+            link="/projects/#ticket-management"
+            image=""
+          />
+          <ProjectCard
+            title="Carbon Emissions Calculator"
+            description="A user-friendly website to calculate carbon emissions for daily commutes."
+            link="/projects/#carbon-calculator"
+            image=""
+          />
         </div>
+          <div>
+          <Link href="/projects" className="px-8 py-4 bg-blue-500 text-white rounded-md font-semibold uppercase hover:bg-blue-600 transition duration-300 mt-8">
+          View All Projects
+        </Link>
+          </div>
       </div>
     </>
   );
---
+++ src/components/partials/ProjectCard.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image';
+
+interface ProjectCardProps {
+    title: string;
+    description: string;
+    link: string;
+    image?: string;
+  }
+
+const ProjectCard: React.FC<ProjectCardProps> = ({ title, description, link, image }) => {
+  return (
+
+    <div className="bg-white shadow-lg rounded-lg p-6 mb-8">
+      <h3 className="text-xl font-semibold mb-2">{title}</h3>
+      <p className="text-gray-600 mb-4">{description}</p>
+      <Link href={link} className="px-4 py-2 bg-blue-500 text-white rounded-md font-semibold uppercase hover:bg-blue-600 transition duration-300">
+          Learn More
+      </Link>
+    </div>
+    )
+};
+
+export default ProjectCard
