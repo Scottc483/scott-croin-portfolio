8ecb8cd7c0bd2d7171231e89caf74352fdb7225f
Scottc483 <scott.c19@live.com>
2023-05-19 17:52:13 -0700

    Contact Intro font color change

--- src/components/ContactIntro.tsx
+++ src/components/ContactIntro.tsx
@@ -3,10 +3,10 @@ import Link from 'next/link'
 const ContactIntro = () => {
   return (
     <div className="flex flex-col py-16 max-w-5xl p-16">
-    <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4">
+    <h2 className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 offWhite">
       Get In Touch
     </h2>
-    <p className="text-lg mb-8">
+    <p className="text-lg mb-8 offWhite">
       Have a project in mind or want to collaborate? I&apos;d love to hear from
       you. Feel free to reach out using the contact form or through the
       provided channels below. Let&apos;s connect and bring your ideas to life.
